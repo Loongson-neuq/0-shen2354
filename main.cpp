db561eb800e7a6d99257258e80551ea7ee7c0fbc
shen linke <2354623738@qq.com>
2024-10-14 11:58:58 +0800

    #include .assignment/test.h // 比较三个数的大小 void CompareThreeNumbers(int a, int b, int c, struct Result *result) {     int min, mid, max;
        // 临时变量，用于交换
        int temp;
    
        // 初始化最小值、中间值和最大值为第一个数
        min = mid = max = a;
    
        if (b < min) {
            mid = min;
            min = b;
        } else if (b > min && b < mid) {
            mid = b;
        }
    
        if (c < min) {
            mid = min;
            min = c;
        } else if (c > min && c < mid) {
            mid = c;
        } else if (c > mid) {
            max = mid;
            mid = c;
        }
        result->min = min;
        result->mid = mid;
        result->max = max;
    }

--- main.cpp
+++ main.cpp
@@ -1,7 +1,31 @@
 #include ".assignment/test.h"
-
 // 比较三个数的大小
-void CompareThreeNumbers(int a, int b, int c, Result &result)
-{
-    // 在这里完成你的代码
+void CompareThreeNumbers(int a, int b, int c, struct Result *result) {
+    int min, mid, max;
+
+    // 临时变量，用于交换
+    int temp;
+
+    // 初始化最小值、中间值和最大值为第一个数
+    min = mid = max = a;
+    
+    if (b < min) {
+        mid = min;
+        min = b;
+    } else if (b > min && b < mid) {
+        mid = b;
+    }
+
+    if (c < min) {
+        mid = min;
+        min = c;
+    } else if (c > min && c < mid) {
+        mid = c;
+    } else if (c > mid) {
+        max = mid;
+        mid = c;
+    }
+    result->min = min;
+    result->mid = mid;
+    result->max = max;
 }
